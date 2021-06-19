2661cffe3cad8ec2cee79862317407164bb4a92e
VatrasCell <vatrascell@googlemail.com>
2021-06-19 16:39:54 +0200

    remove mysqldump.exe

--- src/main/java/application/Database.java
+++ src/main/java/application/Database.java
@@ -1,8 +1,5 @@
 package application;
 
-import java.io.File;
-import java.io.IOException;
-
 public class Database {
 
     protected final static String DB_NAME = "nezr_v8"; // Datenbankname
@@ -20,44 +17,4 @@ public class Database {
     protected static String user = ""; // Login-ID
     protected static String pwd = ""; // Passwort
 
-    @Deprecated
-    protected static String exp_cmd(int art, String name) throws IOException {
-        String ret = "false";
-
-        try {
-            File theDir = new File("exportSQL");
-
-            if (!theDir.exists()) {
-                try {
-                    theDir.mkdir();
-                } catch (SecurityException se) {
-                }
-            }
-
-            String statement = "";
-            switch (art) {
-                case 0:
-                    statement = "cmd /c .\\mysqldump -u " + user + " -p" + pwd + " --no-data " + DB_NAME + " > exportSQL\\"
-                            + name;
-                    break;
-                case 1:
-                    statement = "cmd /c .\\mysqldump -u " + user + " -p" + pwd + " --no-create-info " + DB_NAME
-                            + " > exportSQL\\" + name;
-                    break;
-                case 2:
-                    statement = "cmd /c .\\mysqldump -u " + user + " -p" + pwd + "" + DB_NAME
-                            + " > exportSQL\\" + name;
-                    break;
-            }
-
-            Runtime.getRuntime().exec(statement);
-
-            ret = "true";
-        } catch (Exception e) {
-            e.printStackTrace();
-            //ErrorLog.fehlerBerichtB("ERROR",
-            //		Datenbank.class + ": " + Thread.currentThread().getStackTrace()[1].getLineNumber(), e.getMessage());
-        }
-        return ret;
-    }
 }
